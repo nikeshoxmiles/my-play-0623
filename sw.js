db3b6288fbf68829782b4d169f65bbb0500c3b55
nikeshoxmiles <128239597+nikeshoxmiles@users.noreply.github.com>
2025-06-22 23:28:57 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'jingfeng-plan-v2-cache';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700;900&display=swap',
+  'https://unpkg.com/@phosphor-icons/web'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
